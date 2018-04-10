7594a5f4005d35aa9319a9fbc31cb7ab0cfae49d
Xueshan Bai <xbai10@jhu.edu>
2018-04-10 00:41:28 -0400

    add option templateson VotePage

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./components/pages/HomePage";
 import DonatePage from "./components/pages/DonatePage";
 import SuccessPage from "./components/pages/SuccessPage";
 import VotePage from "./components/pages/VotePage";
+import ThankPage from "./components/pages/ThankPage";
 
 const App = () => (
 <div className="ui container">
@@ -11,6 +12,7 @@ const App = () => (
   <Route path="/donate" exact component={DonatePage} />
   <Route path="/success" exact component={SuccessPage} />
   <Route path="/vote" exact component={VotePage} />
+  <Route path="/thank" exact component={ThankPage} />
 </div>
 );
 
--- react_app/src/components/pages/VotePage.js
+++ react_app/src/components/pages/VotePage.js
@@ -1,27 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import './Vote.css';
 class VotePage extends React.Component {
 
 render() {
-return(
 
-<div className="ui card">
+   
+
+    const formStyle={
+        position: "relative",
+        width: "120%"
+        
+  
+  
+      }
+
+return(
+<div >
+<h2 textAlign="center">Your Voting Options</h2>
+<div className="ui card" style={formStyle}>
     <div className="content">
-      <img src="/assets/images/avatar/large/steve.jpg" className="ui mini right floated image" />
+     
       <div className="header">LovePanda</div>
       <div className="meta">Chengdu, China</div>
-      <div className="description">Steve wants to add you to the group <strong>best friends</strong>
+      <div className="description">There are 50 wild pandas in Chengdu needing money for their protection.
       </div>
     </div>
     <div class="extra content">
       <div class="ui two buttons">
-        <button class="ui green basic button" role="button">Vote</button>
-        <button class="ui red basic button" role="button">Details</button>
+   <Link to="/thank">  
+   <button class="ui green basic button">Vote</button>
+   </Link>
+ 
+        <button class="ui green basic button" role="button">More Details</button>
+        
       </div>
     </div>
  </div>
 
+<div className="ui card" style={formStyle}>
+    <div className="content">
+     
+      <div className="header">HeartBeats</div>
+      <div className="meta">Ohio, USA</div>
+      <div className="description">Save 27 children with heart problems! 
+      </div>
+    </div>
+    <div class="extra content">
+      <div class="ui two buttons">
+      <Link to="/thank"> 
+        <button class="ui green basic button" role="button">Vote</button>
+        </Link>
+        <button class="ui green basic button" role="button">More Details</button>
+      </div>
+    </div>
+ </div>
+
+ <div className="ui card" style={formStyle}>
+    <div className="content">
+     
+      <div className="header">VAD</div>
+      <div className="meta">Africa, Southeast Asia</div>
+      <div className="description">Vitamin A deficiency (VAD) is the leading cause of preventable blindness in children and increases the risk of disease and death from severe infections.
+      </div>
+    </div>
+    <div class="extra content">
+      <div class="ui two buttons">
+      <Link to="/thank"> 
+        <button class="ui green basic button" role="button">Vote</button>
+        </Link>
+        <button class="ui green basic button" role="button">More Details</button>
+      </div>
+    </div>
+ </div>
+</div>
 )
 
 

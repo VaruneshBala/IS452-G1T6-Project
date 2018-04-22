e7226074020d81c6300af5a9eb833e1537da90bd
mudkipium <ewong28@jhu.edu>
2018-04-22 19:47:58 -0400

    Added another test

--- test/testcharity.js
+++ test/testcharity.js
@@ -28,5 +28,18 @@ contract("Test Charity", function(accounts) {
                 expect(res.toString()).to.be.equal("0x1000000000000000000000000000000000000000");
             });
         });
+        it("non-creator cannot add new charity", function() {
+            testContract.addVoteOption("RedCross", "0x2000000000000000000000000000000000000000", {from: accounts[1]});
+            return testContract.votingOptionsCount().then(function(res) {
+                expect(res.toString()).to.be.equal("1");
+            });
+        });
+        it("voting cannot proceed without creator calling startVoting", function() {
+            testContract.startVoting(100000000, {from: accounts[1]});
+            testContract.vote(0, {from: accounts[1]});
+            return testContract.votingOptionVotes(0).then(function(res) {
+                expect(res.toString()).to.be.equal("0");
+            })
+        })
     });
 });

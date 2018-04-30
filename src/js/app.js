64e9c0f283677fe1c25e82ea5ff07eade7cb1bf2
Xueshan Bai <xbai10@jhu.edu>
2018-04-29 21:23:21 -0400

    alert when not enough balance to donate

--- src/js/app.js
+++ src/js/app.js
@@ -77,20 +77,28 @@ web3 = new Web3(App.web3Provider);
  handleDonate: function(event) {
    //if this method is called, the default action of the event will not be triggered
    event.preventDefault();
-
+   //get the input value from donator
    var amount=document.getElementById("amount").value;
+   console.log(amount);
+   //get account
    web3.eth.getAccounts(function(error, accounts) {
    if (error) {
      console.log(error);
    }
+   //get account balance
+   web3.eth.getBalance(accounts[0],function(error, balance) {
+   var balance = web3.fromWei(balance, "ether").toNumber();
+   console.log(balance)
+   if(amount > balance) //if donation amount exceeds account getBalance
+   { console.log("hi")
+     alert("Not enough balance");
+   } else {//donate
 
-   var account = accounts[0]; //how to refer to the user account
-
-   App.contracts.Charity.deployed().then(function(instance) {
-     votingInstance = instance;
-     // Execute voting as a transaction by sending account
-     var value= web3.toWei(amount, "ether");
-     votingInstance.donate({from: account, value: value})
+     App.contracts.Charity.deployed().then(function(instance) {
+       votingInstance = instance;
+       //convert ether to wei
+       var value= web3.toWei(amount, "ether");
+     votingInstance.donate({from: accounts[0], value: value})
      .then(function() {
        //check the charity balance
        votingInstance.getBalance().then(function(res) {
@@ -103,11 +111,13 @@ web3 = new Web3(App.web3Provider);
    }).catch(function(err) {
      console.log(err.message);
    });
+   }
+})
+
  });
 
 
  },
-
 
   handleVote: function(event) {
     //if this method is called, the default action of the event will not be triggered

6a9c6391bd304f47ad0609da6000ab2b8590a9e8
ModWilliam <mod.william@gmail.com>
2018-05-06 11:57:16 -0400

    Explained bug in dispersal. Removed some comments. Fixed hoek version

--- contracts/Charity.sol
+++ contracts/Charity.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.18;
 
 contract Charity {
 
-    address public creator; // TODO: multiple creators/admins?
-    mapping (address => uint) public donations; // TODO: make it easier to see everyone's donations
-    bytes32[] public votingOptions;  // TODO: Convert to bytes32
+    address public creator;
+    mapping (address => uint) public donations;
+    bytes32[] public votingOptions;
     address[] public votingOptionAddresses;
     uint[] public votingOptionVotes;
     uint public votingOptionsCount;
@@ -73,29 +73,36 @@ contract Charity {
 
     // Called after the current voting period ends to donate to the majority vote cause
     function disperse() public returns (bool){
-        votingOptionAddresses[3].transfer(address(this).balance);
-        return true;
+
+        /* votingOptionAddresses[3].transfer(address(this).balance);
+        return true; */
+
+        // NOTE: Currently, there is an issue with ganache that does not allow us to zero out array indices (like in line 93)
+        // So, the actual code is commented out below, while the code to demo is commented above.
+        // The code below passes the tests, so we know it should work.
+        // Source: https://github.com/trufflesuite/ganache-cli/issues/491
+
         // https://ethereum.stackexchange.com/questions/3373/how-to-clear-large-arrays-without-blowing-the-gas-limit
-        //if (now >= endTime) {
-        //    uint maxVotes = 0;
-        //    uint maxIndex = 2**256 - 1;
-        //    for (uint i = 0; i < votingOptionsCount; i++) {
-        //        if (votingOptionVotes[i] > maxVotes) {
-        //            maxIndex = i;
-        //            maxVotes = votingOptionVotes[i];
-        //            votingOptionVotes[i] = 0;
-        //        }
-        //    }
-        //    votingOptionsCount = 0;
-        //    startTime = 2**256 - 1;
-        //    endTime = 2**256 - 1;
-        //    if (maxIndex == 2**256 - 1) {
-        //        return false; //no one voted
-        //    } else {
-        //        votingOptionAddresses[maxIndex].transfer(address(this).balance);
-        //    }
-        //    return true;
-        //}
+        if (now >= endTime) {
+            uint maxVotes = 0;
+            uint maxIndex = 2**256 - 1;
+            for (uint i = 0; i < votingOptionsCount; i++) {
+                if (votingOptionVotes[i] > maxVotes) {
+                    maxIndex = i;
+                    maxVotes = votingOptionVotes[i];
+                    votingOptionVotes[i] = 0;
+                }
+            }
+            votingOptionsCount = 0;
+            startTime = 2**256 - 1;
+            endTime = 2**256 - 1;
+            if (maxIndex == 2**256 - 1) {
+                return false; //no one voted
+            } else {
+                votingOptionAddresses[maxIndex].transfer(address(this).balance);
+            }
+            return true;
+        }
         return false;
     }
 
@@ -120,6 +127,6 @@ contract Charity {
     }
 
     function getAccountBalance(address addr) public view returns(uint) {
-		return addr.balance;
-	}
+		  return addr.balance;
+	  }
 }
